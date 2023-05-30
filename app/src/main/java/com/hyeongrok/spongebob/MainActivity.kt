ee9a04d3476737024d27a7349552e8ec0a5659be
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-30 19:25:17 +0900

    스퐁지밥 웹뷰 띄우기

--- app/src/main/java/com/hyeongrok/spongebob/MainActivity.kt
+++ app/src/main/java/com/hyeongrok/spongebob/MainActivity.kt
@@ -10,14 +10,38 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.web.AccompanistWebChromeClient
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 import com.hyeongrok.spongebob.ui.theme.SpongeBobTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val mainViewModel = MainViewModel()
+        val webViewClient = AccompanistWebViewClient()
+        val webChromeClient = AccompanistWebChromeClient()
         setContent {
-            SpongeBobTheme {
+            val webViewState = mainViewModel.webViewState
+            val webViewNavigator = mainViewModel.webViewNavigator
 
+            SpongeBobTheme {
+                WebView(
+                    state = webViewState,
+                    client = webViewClient,
+                    chromeClient = webChromeClient,
+                    navigator = webViewNavigator,
+                    onCreated = {
+                        with(it) {
+                            settings.run {
+                                javaScriptEnabled = true
+                                domStorageEnabled = true
+                                javaScriptCanOpenWindowsAutomatically = false
+                            }
+                        }
+                    }
+                )
             }
         }
     }
