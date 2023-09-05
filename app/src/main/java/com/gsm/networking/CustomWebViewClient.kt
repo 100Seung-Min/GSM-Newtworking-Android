f9ee09a3e29374ee3a70b430e5a62fe41a933dee
백승민 <bsmin0417@naver.com>
2023-09-05 19:47:20 +0900

    customWebViewClient 생성

---
+++ app/src/main/java/com/gsm/networking/CustomWebViewClient.kt
@@ -0,0 +1,33 @@
+package com.gsm.networking
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import com.google.accompanist.web.AccompanistWebViewClient
+
+class CustomWebViewClient(private val context: Context) : AccompanistWebViewClient() {
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        val intent = if (request?.url.toString().startsWith("mailto:")) {
+            Intent(Intent.ACTION_SENDTO).apply {
+                type = "plain/text"
+                data = request?.url
+            }
+        } else if (request?.url.toString().contains("https://gsm.moip.shop/")) {
+            null
+        } else {
+            Intent(Intent.ACTION_VIEW, request?.url)
+        }
+        intent?.let { context.startActivity(it) }
+        return true
+    }
+}
