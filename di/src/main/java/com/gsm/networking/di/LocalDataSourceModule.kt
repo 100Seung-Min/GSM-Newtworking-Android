dbdc81a046fdb4001cf7cdb79c9817cd6b5f06df
백승민 <bsmin0417@naver.com>
2023-09-11 13:31:46 +0900

    DI모듈 기본 세팅

---
+++ di/src/main/java/com/gsm/networking/di/LocalDataSourceModule.kt
@@ -0,0 +1,10 @@
+package com.gsm.networking.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocalDataSourceModule {
+}
---
+++ di/src/main/java/com/gsm/networking/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.gsm.networking.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    fun provideOkHttpclient(): OkHttpClient = OkHttpClient.Builder()
+        .build()
+
+    @Provides
+    fun provideRetrofitClient(
+        okHttpClient: OkHttpClient,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BUILD_TYPE)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
