670874bf5c1e6d875cc748096bde19f6b671689b
백승민 <bsmin0417@naver.com>
2023-09-11 14:41:02 +0900

    authAPI 로컬 부분 DI 설정

--- di/src/main/java/com/gsm/networking/di/LocalDataSourceModule.kt
+++ di/src/main/java/com/gsm/networking/di/LocalDataSourceModule.kt
@@ -1,5 +1,8 @@
 package com.gsm.networking.di
 
+import com.gsm.networking.data.local.datasource.AuthLocalDataSource
+import com.gsm.networking.data.local.datasource.AuthLocalDataSourceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,4 +10,8 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LocalDataSourceModule {
+    @Binds
+    abstract fun provideAuthLocalDataSource(
+        authLocalDataSourceImpl: AuthLocalDataSourceImpl
+    ): AuthLocalDataSource
 }
--- di/src/main/java/com/gsm/networking/di/PreferenceModule.kt
+++ di/src/main/java/com/gsm/networking/di/PreferenceModule.kt
@@ -1,5 +1,8 @@
 package com.gsm.networking.di
 
+import com.gsm.networking.data.local.preference.AuthPreference
+import com.gsm.networking.data.local.preference.AuthPreferenceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,4 +10,8 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class PreferenceModule {
+    @Binds
+    abstract fun provideAuthPreference(
+        authPreferenceImpl: AuthPreferenceImpl
+    ): AuthPreference
 }
