9111ea7c3b55e6d548a7b345530f6fb6749d4ab3
백승민 <bsmin0417@naver.com>
2023-09-11 13:26:46 +0900

    구글 로그인 버튼 컴포넌트 구현

--- presentation/src/main/java/com/gsm/networking/SignInActivity.kt
+++ presentation/src/main/java/com/gsm/networking/SignInActivity.kt
@@ -5,6 +5,27 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -12,23 +33,61 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 class SignInActivity : ComponentActivity() {
     private lateinit var googleSignInClient: GoogleSignInClient
     private val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestServerAuthCode("")
+        .requestServerAuthCode(BuildConfig.CLIENT_ID)
         .requestEmail()
         .build()
-    private val signInLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        val result = GoogleSignIn.getSignedInAccountFromIntent(it.data).result
-        if (result.email?.endsWith("@gsm.hs.kr") == true) {
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == -1) {
+                val result = GoogleSignIn.getSignedInAccountFromIntent(it.data).result
+                if (result.email?.endsWith("@gsm.hs.kr") == true) {
 
-        } else {
-            googleSignInClient.signOut()
-            Toast.makeText(this, "@gsm.hs.kr 계정으로만 접속 가능합니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    googleSignInClient.signOut()
+                    Toast.makeText(this, "@gsm.hs.kr 계정으로만 접속 가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         setContent {
-            signInLauncher.launch(googleSignInClient.signInIntent)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "GSM\nNetworking", textAlign = TextAlign.Center)
+                Spacer(modifier = Modifier.height(40.dp))
+                GoogleSignInButton {
+                    signInLauncher.launch(googleSignInClient.signInIntent)
+                }
+                Spacer(modifier = Modifier.height(40.dp))
+                Text(text = "*GSM 계정으로만 접속 가능합니다.")
+            }
         }
+    }
+}
+
+@Composable
+private fun GoogleSignInButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .background(color = Color(0xFFF5F6F8), shape = RoundedCornerShape(40.dp))
+            .padding(vertical = 20.dp, horizontal = 12.dp)
+            .clickable(onClick = onClick),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = R.drawable.ic_google),
+            contentDescription = "googleIcon"
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "구글로 로그인")
     }
 }
