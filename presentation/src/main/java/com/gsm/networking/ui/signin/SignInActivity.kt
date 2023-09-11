4fb7180bb9a5f6216aaae2eec4c30c323637d017
백승민 <bsmin0417@naver.com>
2023-09-11 20:46:24 +0900

    구글 로그인 화면 폰트 적용

--- presentation/src/main/java/com/gsm/networking/ui/signin/SignInActivity.kt
+++ presentation/src/main/java/com/gsm/networking/ui/signin/SignInActivity.kt
@@ -26,14 +26,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.gsm.networking.BuildConfig
 import com.gsm.networking.R
 import com.gsm.networking.ui.main.MainActivity
+import com.gsm.networking.ui.theme.GSMNetworkingTheme
 import com.gsm.networking.viewmodel.SignInViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -63,18 +64,20 @@ class SignInActivity : ComponentActivity() {
         observeLoginState()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(painter = painterResource(id = R.drawable.ic_gsm_networking), contentDescription = "gsmNetworking")
-                Spacer(modifier = Modifier.height(40.dp))
-                GoogleSignInButton {
-                    signInLauncher.launch(googleSignInClient.signInIntent)
+            GSMNetworkingTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(painter = painterResource(id = R.drawable.ic_gsm_networking), contentDescription = "gsmNetworking")
+                    Spacer(modifier = Modifier.height(80.dp))
+                    GoogleSignInButton {
+                        signInLauncher.launch(googleSignInClient.signInIntent)
+                    }
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Text(text = "*GSM 계정으로만 접속 가능합니다.", color = Color(0xFF828387), style = GSMNetworkingTheme.typography.subtitle)
                 }
-                Spacer(modifier = Modifier.height(40.dp))
-                Text(text = "*GSM 계정으로만 접속 가능합니다.", color = Color(0xFF828387))
             }
         }
     }
@@ -111,6 +114,6 @@ private fun GoogleSignInButton(
             contentDescription = "googleIcon"
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Text(text = "구글로 로그인")
+        Text(text = "구글로 로그인", color = Color(0xFF828387), style = GSMNetworkingTheme.typography.button.copy(fontSize = 17.sp))
     }
 }
