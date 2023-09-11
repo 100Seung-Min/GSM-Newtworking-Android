00a93e77cc89b26b294b0c302eb77ff6e52642f8
백승민 <bsmin0417@naver.com>
2023-09-11 11:06:24 +0900

    project단 buildSrc 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath(Dependency.GradlePlugin.GRADLE_ANDROID)
+        classpath(Dependency.GradlePlugin.GRADLE_KOTLIN)
+        classpath(Dependency.GradlePlugin.GRADLE_HILT)
     }
 }
 
