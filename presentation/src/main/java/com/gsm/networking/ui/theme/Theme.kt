a83190b69270564e4b5a6c4b64234a53f4cd6a06
백승민 <bsmin0417@naver.com>
2023-09-11 20:46:12 +0900

    폰트 세팅

--- presentation/src/main/java/com/gsm/networking/ui/theme/Theme.kt
+++ presentation/src/main/java/com/gsm/networking/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -25,22 +26,11 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun GSMNetworkingTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -62,9 +52,18 @@ fun GSMNetworkingTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalGSMNetworkingTypography provides defaultTypography
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content
+        )
+    }
+}
+
+object GSMNetworkingTheme {
+    val typography: GSMNetworkingTypography
+        @Composable
+        get() = LocalGSMNetworkingTypography.current
 }
--- presentation/src/main/java/com/gsm/networking/ui/theme/Type.kt
+++ presentation/src/main/java/com/gsm/networking/ui/theme/Type.kt
@@ -1,34 +1,99 @@
 package com.gsm.networking.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.gsm.networking.R
+import javax.annotation.concurrent.Immutable
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+internal val pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+)
+
+@Immutable
+data class GSMNetworkingTypography(
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val h3: TextStyle,
+    val h4: TextStyle,
+    val subtitle: TextStyle,
+    val body1: TextStyle,
+    val body2: TextStyle,
+    val button: TextStyle,
+    val caption: TextStyle,
+    val overLine: TextStyle,
+)
+
+internal val LocalGSMNetworkingTypography = staticCompositionLocalOf {
+    GSMNetworkingTypography(
+        h1 = TextStyle.Default,
+        h2 = TextStyle.Default,
+        h3 = TextStyle.Default,
+        h4 = TextStyle.Default,
+        subtitle = TextStyle.Default,
+        body1 = TextStyle.Default,
+        body2 = TextStyle.Default,
+        button = TextStyle.Default,
+        caption = TextStyle.Default,
+        overLine = TextStyle.Default
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+}
+
+val defaultTypography = GSMNetworkingTypography(
+    h1 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 48.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    h2 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+    ),
+    h4 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+    ),
+    subtitle = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = pretendard,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 16.sp,
+    ),
+    body2 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+    ),
+    button = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+    ),
+    caption = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+    ),
+    overLine = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp,
     )
-    */
 )
