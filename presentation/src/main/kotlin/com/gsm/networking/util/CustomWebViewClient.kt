2cea885f89eee031794ef5c541e12abc7f7a3ffa
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-11-17 14:29:54 +0900

    구글 로그인 오류 해결 및 쿠키 동기화

--- presentation/src/main/kotlin/com/gsm/networking/util/CustomWebViewClient.kt
+++ presentation/src/main/kotlin/com/gsm/networking/util/CustomWebViewClient.kt
@@ -2,33 +2,37 @@ package com.gsm.networking.util
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
+import android.net.Uri
+import android.webkit.CookieManager
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import com.google.accompanist.web.AccompanistWebViewClient
-import com.gsm.networking.BuildConfig
+import com.google.accompanist.web.WebViewNavigator
 
-class CustomWebViewClient(private val context: Context) : AccompanistWebViewClient() {
-    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-        super.onPageStarted(view, url, favicon)
-    }
-
+class CustomWebViewClient(
+    private val context: Context,
+    private val webViewNavigator: WebViewNavigator,
+) : AccompanistWebViewClient() {
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
+        CookieManager.getInstance().flush()
     }
 
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-        val intent = if (request?.url.toString().startsWith("mailto:")) {
+        val url = request?.url?.toString() ?: ""
+        if (url.contains("accounts.google.com") && url.contains("gsm-networking").not()) {
+            webViewNavigator.reload()
+            return true
+        }
+        val intent = if (url.startsWith("mailto:")) {
             Intent(Intent.ACTION_SENDTO).apply {
                 type = "plain/text"
-                data = request?.url
+                data = Uri.parse(url)
             }
-        } else if (request?.url.toString()
-                .let { it.contains("gsm-networking") && it.contains("notion").not() }
-        ) {
+        } else if (url.contains("gsm-networking") && url.contains("notion").not()) {
             return super.shouldOverrideUrlLoading(view, request)
         } else {
-            Intent(Intent.ACTION_VIEW, request?.url)
+            Intent(Intent.ACTION_VIEW, Uri.parse(url))
         }
         intent.let { context.startActivity(it) }
         return true
